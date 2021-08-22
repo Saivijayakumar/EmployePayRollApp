a459a6ef45b129b0a2fa4447ef90343b7982d8e6
Sai Vijayakumar Bollu <saivijayakumar.b@tvsnext.io>
2021-08-22 19:08:36 +0530

    [SaiVijayaKumar]Adding: Seting Event Listener and validating value through get and method

--- js/EmpFormJS.js
+++ js/EmpFormJS.js
@@ -1,6 +1,22 @@
-const salary = document.querySelector('#salary');
-const salaryOutput = document.querySelector('.salary-output');
-salaryOutput.textContent = salary.value;
-salary.addEventListener('input',function(){
+window.addEventListener('DOMContentLoaded', (event)=>{
+    const name = document.querySelector('#name');
+    const textError = document.querySelector('.text-error');
+    name.addEventListener('input', function () {
+        if (name.value.length == 0) {
+            textError.textContent = "";
+            return;
+        }
+        try {
+            (new EmployeePayrollData()).name = name.value;
+            textError.textContent = "";
+        } catch (e) {
+            textError.textContent = e;
+        }
+    });
+    const salary = document.querySelector('#salary');
+    const salaryOutput = document.querySelector('.salary-output');
     salaryOutput.textContent = salary.value;
+    salary.addEventListener('input', function () {
+        salaryOutput.textContent = salary.value;
+    });
 });

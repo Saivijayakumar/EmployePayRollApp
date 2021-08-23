12e26e4681550d0fb3b8d3e5b3c2bfd9e6de686e
Sai Vijayakumar Bollu <saivijayakumar.b@tvsnext.io>
2021-08-23 23:06:49 +0530

    [SaiVijayaKumar]Adding: displaying data from local storage in table formate

--- js/EmpHomejs.js
+++ js/EmpHomejs.js
@@ -1,12 +1,20 @@
+let empPayrollList;
 window.addEventListener('DOMContentLoaded', (event) => {
+    empPayrollList = getEmployeePayrollDataFromLocalStorage();
+    document.querySelector(".emp-count").textContent = empPayrollList.length;
     createInnerHtml();
+    localStorage.removeItem('editEmp');
 });
+
+const getEmployeePayrollDataFromLocalStorage = () => {
+    return localStorage.getItem('EmployeePayrollList') ? JSON.parse(localStorage.getItem('EmployeePayrollList')) : [];
+}
 
 //Template literal ES6 feature
 const createInnerHtml = () => {
     const headerHtml = "<th></th><th>Name</th><th>Gender</th><th>Department</th><th>Salry</th><th>Start Date</th><th>Actions</th>";
+    if(empPayrollList.length == 0) return;
     let innerHtml = `${headerHtml}`;
-    let empPayrollList = creteEmployeePayrollJSON();
     for(const empPayrollData of empPayrollList){
         innerHtml = `${innerHtml}
         <tr>
@@ -15,7 +23,7 @@ const createInnerHtml = () => {
             <td>${empPayrollData._gender}</td>
             <td>${getDeptHtml(empPayrollData._department)}</td>
             <td>${empPayrollData._salary}</td>
-            <td>${empPayrollData._startDate}</td>
+            <td>${(empPayrollData._startDate)}</td>
             <td>
             <img id="${empPayrollData._id}" onclick="remove(this)" alt="delete" src="../assests/homepage/delete-black-18dp.svg">
             <img id="${empPayrollData._id}" onclick="update(this)" alt="Update" src="../assests/homepage/create-black-18dp.svg">
@@ -32,34 +40,4 @@ const getDeptHtml = (depList)=>{
         deptHtml = `${deptHtml} <div class='dept-lable'>${dept}</div>`
     }
     return deptHtml;
-}
-
-const creteEmployeePayrollJSON = () => {
-    let empPayrollListLocal = [
-        {
-            "_name": "Mohit kumar new",
-            "_gender": "male",
-            "_department": [
-                "HR"
-            ],
-            "_salary": "30000",
-            "_startDate": "1 Avg 2021",
-            "_note": "",
-            "_id": new Date().getTime(),
-            "_profile": "../assests/Ellipse -8.png"
-        },
-        {
-            "_name": "Mohit kumar",
-            "_gender": "male",
-            "_department": [
-                "HR", "Sales"
-            ],
-            "_salary": "30000",
-            "_startDate": "12 Avg 2021",
-            "_note": "",
-            "_id": new Date().getTime() + 1,
-            "_profile": "../assests/Ellipse -7.png"
-        }
-    ];
-    return empPayrollListLocal;
 }
